8d67de71bf5e63afd453fdfcf6e524b61d98c1c6
Minki Kang <zzxc1133@gmail.com>
2025-08-01 11:39:58 -0700

    Update train_cot.sh
    Compatible with huggingface datasets

--- scripts/training/train_cot.sh
+++ scripts/training/train_cot.sh
@@ -3,9 +3,7 @@ set -x
 
 MODEL=$1
 DATASETS=(
-    "logs/qa_results/vllm/Qwen_Qwen2.5-32B-Instruct/hotpotqa_1000_20250402_train/filtered_data/Qwen2.5-32B-Instruct_temp=0.0_seed=42_type=reasoning_filtered.jsonl"
-    "logs/qa_results/vllm/Qwen_Qwen2.5-32B-Instruct/math_1000_20250414_train/filtered_data/Qwen2.5-32B-Instruct_temp=0.0_seed=42_type=reasoning_filtered.jsonl"
-    "logs/qa_results/vllm/Qwen_Qwen2.5-32B-Instruct/math_medium_1000_20250430_train/filtered_data/Qwen2.5-32B-Instruct_temp=0.5_seed=42_type=reasoning_filtered.jsonl"
+    "agent-distillation/Qwen2.5-32B-Instruct_cot_trajectories_2k"
 )
 SAVENAME="qwen2.5_32B_teacher"
 JOINED_DATASETS=$(IFS=" "; echo "${DATASETS[*]}")
